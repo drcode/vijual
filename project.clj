4482882baf9fc657d09629b8235ceb10dfe64d32
Conrad Barski <conrad@conrad-inspiron-1420.(none)>
2010-01-30 22:07:08 -0500

    merged graphics code into main file to resolve clojar issue

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"] 
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [leiningen/lein-swank "1.1.0"]])
+                     [leiningen/lein-swank "1.1.0"]]
+  :repositories [["clojars" "http://clojars.org/repo"]])
