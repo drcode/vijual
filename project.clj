9b3f5483d56c8e9c2942f6c12aacfbeb5c96264b
Dan Boykis <danboykis@gmail.com>
2016-03-20 16:47:17 -0400

    Upgrading lein dependencies
    Upgrading to Clojure 1.8
    Took out deprecated clojure-contrib in favor of math.numeric-tower
    Took out deprecated seq-utils and reimplemented positions in terms
    of map-indexed

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject vijual "0.1.0-SNAPSHOT" 
   :description "A Graph Layout Library For Clojure" 
-  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"] 
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [leiningen/lein-swank "1.1.0"]]
   :repositories [["clojars" "http://clojars.org/repo"]])
